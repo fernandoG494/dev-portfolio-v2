d35ba712288379f9403a807bba4f5417d3452f7c
fernandoG494 <darkogarcia3@gmail.com>
2024-05-26 21:12:03 -0600

    Icon images styles

--- src/components/IconSelector.tsx
+++ src/components/IconSelector.tsx
@@ -1,17 +1,23 @@
+import { useContext } from "react";
 import PersonIcon from "@mui/icons-material/Person";
-import PhonelinkIcon from "@mui/icons-material/Phonelink";
 import StorageIcon from "@mui/icons-material/Storage";
+import PhonelinkIcon from "@mui/icons-material/Phonelink";
 
 import { ISection } from "../interfaces/data";
+import { PlaceThemeContext } from "../interfaces/theme";
+
+import "../styles/components/IconSelector.scss";
 
 const IconSelector = ({ title }: ISection) => {
+  const { themeType } = useContext(PlaceThemeContext);
+
   switch (title) {
     case "About me":
-      return <PersonIcon />;
+      return <PersonIcon className={`Icon-color ${themeType}`} />;
     case "Front end projects":
-      return <PhonelinkIcon />;
+      return <PhonelinkIcon className={`Icon-color ${themeType}`} />;
     case "Back end projects":
-      return <StorageIcon />;
+      return <StorageIcon className={`Icon-color ${themeType}`} />;
   }
 };
 
