dd4c5fee726bf769e6044005ca306530e1d59412
fernandoG494 <darkogarcia3@gmail.com>
2024-05-26 21:54:26 -0600

    Active class in sections

--- src/components/IconSelector.tsx
+++ src/components/IconSelector.tsx
@@ -1,8 +1,10 @@
 import { useContext } from "react";
+import { useSelector } from "react-redux";
 import PersonIcon from "@mui/icons-material/Person";
 import StorageIcon from "@mui/icons-material/Storage";
 import PhonelinkIcon from "@mui/icons-material/Phonelink";
 
+import { RootState } from "../store";
 import { ISection } from "../interfaces/data";
 import { PlaceThemeContext } from "../interfaces/theme";
 
@@ -10,23 +12,35 @@ import "../styles/components/IconSelector.scss";
 
 const IconSelector = ({ title }: ISection) => {
   const { themeType } = useContext(PlaceThemeContext);
+  const actualPage = useSelector((state: RootState) => state.layout.actualPage);
+
+  const getActiveClass = (page: string) => {
+    if (page === "/" || page === "/about-me") {
+      return title === "About me" ? "active" : "";
+    }
+    if (page === "/front-projects") {
+      return title === "Front end projects" ? "active" : "";
+    }
+    if (page === "/back-projects") {
+      return title === "Back end projects" ? "active" : "";
+    }
+    return "";
+  };
+
+  const activeClass = getActiveClass(actualPage);
 
   switch (title) {
     case "About me":
       return (
-        <PersonIcon className={`Icon-color ${themeType} ${true && "active"}`} />
+        <PersonIcon className={`Icon-color ${themeType} ${activeClass}`} />
       );
     case "Front end projects":
       return (
-        <PhonelinkIcon
-          className={`Icon-color ${themeType} ${true && "active"}`}
-        />
+        <PhonelinkIcon className={`Icon-color ${themeType} ${activeClass}`} />
       );
     case "Back end projects":
       return (
-        <StorageIcon
-          className={`Icon-color ${themeType} ${true && "active"}`}
-        />
+        <StorageIcon className={`Icon-color ${themeType} ${activeClass}`} />
       );
   }
 };
